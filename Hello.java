f9fbd98a70c422aa05ddb920e84b41c049388e4c
111nawaz <mi12345in@gmail.com>
2025-12-05 14:02:05 +0000

    i have updated the file

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello Jenkins! Java project is running successfully!");
+        System.out.println("Hello Jenkins! My name is nawaz and  Java project is running successfully!");
     }
 }
 
