e7757a8765bf50c37c683abd1efec43c3f29b427
111nawaz <mi12345in@gmail.com>
2025-12-05 14:28:05 +0000

    third commit done

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello Jenkins! My name is nawaz and  Java project is running successfully!");
+        System.out.println("Hello Jenkins! My name is nawaz and iam best in the world and my  Java project is running successfully!");
     }
 }
 
